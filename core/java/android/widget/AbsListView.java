75300ef7bf334b5e1c9ff6622b70cff7ac1b5126
dkati <dkatikaridis@gmail.com>
2018-10-26 01:19:06 +0300

    core:Speed up the scroll animation
    Change-Id: Iecca204aa46db0b90e39af6b8d2e2e9aa67ec7f1

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4900,6 +4900,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
+            mScroller.setFriction(0.006f);
         }
 
         void start(int initialVelocity) {
