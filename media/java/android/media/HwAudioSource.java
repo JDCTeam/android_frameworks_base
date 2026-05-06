52adca5bd933075c2b0aae29007f4568410eca3c
Slavey Karadzhov <slav@attachix.com>
2026-05-06 10:47:58 +0200

    Attempt to fix compilation issue with latest LineageOS code.

--- media/java/android/media/HwAudioSource.java
+++ media/java/android/media/HwAudioSource.java
@@ -140,8 +140,9 @@ public class HwAudioSource extends PlayerBase {
      */
     public void start() {
         Preconditions.checkState(!isPlaying(), "HwAudioSource is currently playing");
+        final AudioPort audioPort = mAudioDeviceInfo.getPort();
         mNativeHandle = AudioSystem.startAudioSource(
-                mAudioDeviceInfo.getPort().activeConfig(),
+                audioPort.activeConfig(),
                 mAudioAttributes);
         if (isPlaying()) {
             // FIXME: b/174876389 clean up device id reporting
